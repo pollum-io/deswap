5da01568d8e6aea7d57d1ea6d8e8812f509ae8a1
thomga <gabriel.thom02@gmail.com>
2025-01-15 19:04:05 +0100

    fix: eslint error

--- app/api/swap/route.ts
+++ app/api/swap/route.ts
@@ -1,5 +1,5 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 export const runtime = 'edge';
 export function GET(request: Request) {
-    return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+    return new Response(`Hello from ${request}`);
 }
